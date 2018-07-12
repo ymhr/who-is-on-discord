a32efc1b9520a8667c12bf31eeb2b7663021eebc
James Martin <jamesmartin442@gmail.com>
2018-07-12 23:39:45 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const discord = new Discord.Client();
+
+const TelegramBot = require('node-telegram-bot-api');
+const telegram = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+
+const channelId = process.env.TELEGRAM_CHANNEL_ID;
+
+
+discord.on('voiceStateUpdate', (oldMember, newMember) => {
+    const newUserChannel = newMember.voiceChannel;
+    const oldUserChannel = oldMember.voiceChannel;
+    const userName = newMember.user.username;
+
+    if(typeof oldUserChannel === 'undefined' && typeof newUserChannel !== 'undefined') {
+        console.log('joined', newMember.user.username);
+        telegram.sendMessage(channelId, `${userName} is on discord`);
+    } else if (typeof newUserChannel === 'undefined') {
+        telegram.sendMessage(channelId, `${userName} left discord`);
+    }
+});
+
+discord.login(process.env.DISCORD_TOKEN);
